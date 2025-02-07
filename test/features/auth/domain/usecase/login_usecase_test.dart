6ec2d2db18725bdfad6a2c3c16fa463541b0b4fe
Rabins-Khanal <sjlkhanal@gmail.com>
2025-02-07 17:17:06 +0545

    login usecase modified

--- test/features/auth/domain/usecase/login_usecase_test.dart
+++ test/features/auth/domain/usecase/login_usecase_test.dart
@@ -1,7 +1,39 @@
+import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:uni_football_app/core/error/failure.dart';
+import 'package:uni_football_app/features/auth/domain/usecase/login_usecase.dart';
+
+import 'repository.mock.dart';
 
 void main() {
-  testWidgets('login usecase ...', (tester) async {
-    // TODO: Implement test
+  late MockAuthRepository authRepository;
+  late LoginUseCase loginUseCase;
+
+  setUp(() {
+    authRepository = MockAuthRepository();
+    loginUseCase = LoginUseCase(authRepository);
+  });
+
+  final params = LoginParams(username: 'testuser', password: 'password123');
+
+  test('should return token on successful login', () async {
+    when(() => authRepository.loginUser(any(), any()))
+        .thenAnswer((_) async => Right('mock_token'));
+
+    final result = await loginUseCase(params);
+    expect(result, Right('mock_token'));
+    verify(() => authRepository.loginUser(params.username, params.password))
+        .called(1);
+  });
+
+  test('should return ApiFailure on login error', () async {
+    when(() => authRepository.loginUser(any(), any())).thenAnswer((_) async =>
+        Left(ApiFailure(message: 'Login failed', statusCode: 401)));
+
+    final result = await loginUseCase(params);
+    expect(result, Left(ApiFailure(message: 'Login failed', statusCode: 401)));
+    verify(() => authRepository.loginUser(params.username, params.password))
+        .called(1);
   });
 }
---
+++ test/features/auth/domain/usecase/repository.mock.dart
@@ -0,0 +1,5 @@
+import 'package:mocktail/mocktail.dart';
+import 'package:uni_football_app/features/auth/domain/repository/auth_repository.dart';
+
+// Mock class for AuthRepository
+class MockAuthRepository extends Mock implements IAuthRepository {}
