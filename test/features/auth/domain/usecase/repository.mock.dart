52ec7d335ce9ceec812d8f883f7c70aa701b2d96
Rabins-Khanal <sjlkhanal@gmail.com>
2025-02-07 18:35:23 +0545

    added news UI

--- test/features/auth/domain/usecase/repository.mock.dart
+++
@@ -1,5 +0,0 @@
-import 'package:mocktail/mocktail.dart';
-import 'package:uni_football_app/features/auth/domain/repository/auth_repository.dart';
-
-// Mock class for AuthRepository
-class MockAuthRepository extends Mock implements IAuthRepository {}
